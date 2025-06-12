970df41930cf0601865eccaeb16dabd4c688c0e2
xiinthanh <xiinthanh@gmail.com>
2025-06-12 17:03:46 +0700

    change name to test deploy

--- src/main/java/com/ouroboros/pestadiumbookingbe/controller/BookingController.java
+++ src/main/java/com/ouroboros/pestadiumbookingbe/controller/BookingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/bookings")
+@RequestMapping("/api/create-booking")
 public class BookingController {
 
     @Autowired
