4ee107e0e9375a31836c9d09d1d7dc72068d9b9f
xiinthanh <xiinthanh@gmail.com>
2025-06-13 01:11:12 +0700

    push to test deploy

--- src/main/java/com/ouroboros/pestadiumbookingbe/model/Booking.java
+++ src/main/java/com/ouroboros/pestadiumbookingbe/model/Booking.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+
 @Entity
 @Table(name = "bookings", uniqueConstraints = {
     @UniqueConstraint(columnNames = {"sportHallId", "bookingDate", "timeSlotId"})
