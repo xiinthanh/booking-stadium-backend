b74e9fd83c3f2f5f376fefc33a7c492ed64be7f2
xiinthanh <xiinthanh@gmail.com>
2025-06-13 20:37:42 +0700

    resolve undefined code

--- src/main/java/com/ouroboros/pestadiumbookingbe/controller/ProfilesController.java
+++ src/main/java/com/ouroboros/pestadiumbookingbe/controller/ProfilesController.java
@@ -1,3 +1,7 @@
+package com.ouroboros.pestadiumbookingbe.controller;
+
+import com.ouroboros.pestadiumbookingbe.model.Profile;
+import com.ouroboros.pestadiumbookingbe.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
